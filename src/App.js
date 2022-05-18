7a9bcaa605e4129c8f81c0cf8038ecde6761ab1e
brunovmc <bruno.vidigal.coriolano@gmail.com>
2022-05-18 16:20:58 -0300

    card component created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 // import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -9,10 +10,8 @@ class App extends Component {
       monsters: [],
       searchField:'',
     };
-    console.log('constructor');
   }
   componentDidMount(){
-    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((users) =>
@@ -31,7 +30,6 @@ class App extends Component {
     });
   }
   render(){
-    console.log('render');
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
     const filteredMonsters = monsters.filter((monster)=>{
@@ -44,15 +42,8 @@ class App extends Component {
           type='search' 
           placeholder='search monsters' 
         />
-        {
-          filteredMonsters.map((monster) => {
-            return (
-              <div key={monster.id}>
-                <h1>{monster.name}</h1>
-              </div>
-            );
-          })
-        }
+        <CardList monsters={filteredMonsters}/>
+
       </div>
     );
   }
