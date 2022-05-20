65446699d4e6d42509ac3e167d399ca35f25b25d
brunovmc <bruno.vidigal.coriolano@gmail.com>
2022-05-20 16:46:03 -0300

    add images

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,9 +4,13 @@ class CardList extends Component {
     render(){
         const {monsters} = this.props;
         return (
-        <div>
+        <div className='card-list'>
             {monsters.map((monster) =>(
-                <h1 key={monster.id}>{monster.name}</h1>
+                <div className='card-container' key={monster.id}>
+                    <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt={`monster ${monster.name}`} />
+                    <h2>{monster.name}</h2>
+                    <p>{monster.email}</p>
+                </div>
             ))}
         </div>
         );
